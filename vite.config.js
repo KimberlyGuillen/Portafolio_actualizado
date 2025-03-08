50acd645f16b4cd923492639a8249717e072c6f7
KimberlyGuillen <159837432+KimberlyGuillen@users.noreply.github.com>
2025-03-08 16:38:01 -0600

    listo

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,58 @@
-import path, { resolve } from 'node:path';
-import { defineConfig } from 'vite';
-import * as glob from 'glob';
-import htmlPurge from 'vite-plugin-purgecss';
-import handlebars from 'vite-plugin-handlebars';
-import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import path, {resolve} from 'node:path'
+import { defineConfig } from 'vite'
+import * as glob from 'glob'
+import htmlPurge from 'vite-plugin-purgecss'
+import handlebars from 'vite-plugin-handlebars'
+import {ViteMinifyPlugin} from 'vite-plugin-minify'
 
-const obtenerEntradas = () => {
+const obtenerEntradas = ()=>{
     return Object.fromEntries(
-        glob.sync('./**/*.html', {
-            ignore: ['./dist/**', './node_modules/**'],
-        }).map(fileData => [
-            fileData.slice(0, fileData.length - path.extname(fileData).length),
-            resolve(__dirname, fileData),
-        ])
+        [
+            ...glob.sync(
+                './**/*.html',
+                {
+                    ignore: [
+                        './dist/**',
+                        './node_modules/**'
+                    ]
+                }
+            ).map(
+                fileData => [
+                    fileData.slice(0, fileData.length - path.extname(fileData).length),
+                    resolve(__dirname, fileData)
+                ]
+            )
+        ]
     );
-};
+}
+
 
 export default defineConfig({
     appType: 'mpa',
     base: process.env.DEPLOY_BASE_URL,
-    publicDir: 'public', // Asegura que los PDFs se copien a /dist
     build: {
         rollupOptions: {
-            input: obtenerEntradas(),
+            input: obtenerEntradas()
         },
-        minify: true,
+        minify: true
     },
     plugins: [
         handlebars({
             partialDirectory: resolve(__dirname, 'partials'),
-            context: pagePath => ({}),
+            context : (pagePath)=> {
+                return {}
+            }
         }),
-        htmlPurge({
-            content: ['./index.html', './proyectos.html'], 
-        }),
-        ViteMinifyPlugin(),
-    ],
+        htmlPurge({}),
+        ViteMinifyPlugin()
+    ]
 });
+
+/**
+ 
+✓ 2 modules transformed.
+dist/index.html                 1.71 kB │ gzip: 0.63 kB
+dist/assets/index-DMCu4TLF.css  5.83 kB │ gzip: 1.22 kB
+✓ built in 251ms
+
+ */
